e98e9fe8f9e320269d5366f35b8055377891c525
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2024-11-02 22:27:35 -0300

    chore: productions configs

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV === 'test') {
 }
 
 const envSchema = z.object({
-  PORT: z.string().default('3333'),
+  PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string()
 })
